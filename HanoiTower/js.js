2709b17fe35f73b533b58e29e9cc74c520d168be
Phanquan <phanquan@Techmasters-iMac-6.local>
2017-05-18 15:09:28 +0700

    update full top

--- HanoiTower/js.js
+++ HanoiTower/js.js
@@ -5,7 +5,7 @@ class Disk {
 
 		//set the co-ordinate (x,y) and the height for the object disks
 	}
-	draw(svgInput, attrX, attrY, attrWidth, attrHeight, attrClass, x_, y_, height) {
+	drawDisk(svgInput, attrX, attrY, attrWidth, attrHeight, attrClass, x_, y_, height) {
 		svgInput.append('rect')
 			.attr('x', attrX)
 			.attr('y', attrY)
@@ -23,9 +23,8 @@ class Tower {
 		this.name = nameTowers
 		this.arrDisk = arrDisk
 	}
-	draw(svgInput, attrX1, attrY1, attrX2, attrY2) {
-		svgInput
-			.insert('line', ':first-child')
+	drawTower(svgInput, attrX1, attrY1, attrX2, attrY2) {
+		svgInput.insert('line', ':first-child')
 			.attr('x1', attrX1)
 			.attr('y1', attrY1)
 			.attr('x2', attrX2)
@@ -37,9 +36,17 @@ class Tower {
 
 class GameEngine {
 	constructor() {
-		this.data = []
-		this.count = 0
-		this.step = {}
+		this.count = 0 //to count the moves
+		this.data = [] //main data
+		this.step = {} //steps which describe the movement of the disks
+		this.distenceBetweenTowers = 0
+		this.begin_x_ = 0
+		this.begin_y = 0
+		this.countDisks = 0
+		this.new_x = 0
+		this.new_y = 0
+		this.pickUpHeight = 0
+
 	}
 	move(n, a, b, c) {
 		if (n > 0) {
@@ -58,6 +65,56 @@ class GameEngine {
 			this.move(n - 1, b, a, c)
 		}
 		return this.data
+	}
+
+	//get the distance between towers
+	get_distance(tower1, tower2) {
+		switch (true) {
+			case (tower1 === 'tower1' && tower2 === 'tower2') || (tower1 === 'tower2' && tower2 === 'tower3'):
+				return p.disNearestTower
+			case (tower1 === 'tower2' && tower2 === 'tower1') || (tower1 === 'tower3' && tower2 === 'tower2'):
+				return -p.disNearestTower
+			case (tower1 === 'tower1' && tower2 === 'tower3'):
+				return p.disFurthestTower
+			default:
+				return -p.disFurthestTower
+		}
+	}
+
+	//update the disks array in tower after being moved
+	update_disk(disk, t1, t2) {
+		t1.arrDisk.shift()
+		t2.arrDisk.unshift(disk)
+	}
+
+	//animation method
+	animateDisk(diskLength,diskHeight,animationDelay,animationDuration) {
+		for (var i = 0; i < data.length; i++) {
+			this.distenceBetweenTowers = this.get_distance(data[i].fromTower.name, data[i].toTower.name) // x = distence between the fromTower and the toTower
+			this.begin_x = data[i].diskToPick.x_ //get the x_ of object disk 
+			this.begin_y = data[i].diskToPick.y_ //get the y_ of object disk 
+			this.countDisks = data[i].toTower.arrDisk.length // get the number of disks in the toTower 
+			this.new_x = this.begin_x + this.distenceBetweenTowers //set the new x_ (destination) for the disk
+			this.new_y = diskLength * diskHeight - (this.countDisks * diskHeight) - this.begin_y //set the new y_ for the disk
+			this.pickUpHeight = -data[i].diskToPick.height //set the direction to move the disk,minus for up,plus for down
+
+			//update the current disks on each towers
+			this.update_disk(data[i].diskToPick, data[i].fromTower, data[i].toTower)
+
+			//the actual animation
+			d3.selectAll('.' + data[i].diskToPick.name)
+				.transition()
+				.delay(i * animationDelay)
+				.duration(animationDuration)
+				.attr('transform', 'translate(' + this.begin_x + ',' + this.pickUpHeight + ')')
+				.transition()
+				.attr('transform', 'translate(' + this.new_x + ',' + this.pickUpHeight + ')')
+				.transition()
+				.attr('transform', 'translate(' + this.new_x + ',' + this.new_y + ')')
+
+			//update the x ordinate of the disks after being moved
+			data[i].diskToPick.x_ += this.distenceBetweenTowers
+		}
 	}
 }
 
@@ -87,7 +144,7 @@ for (var j = 1; j <= n; j++) {
 	diskArr.push(new Disk('disk' + j, j))
 
 	//draw the disk that was just defined
-	diskArr[j - 1].draw(
+	diskArr[j - 1].drawDisk(
 		svg, //svg argument
 		(n - j) * p.diskHeight / 2 + p.diskHeight * 2, //.attr('x',
 		j * p.diskHeight + 2 * p.diskHeight, //attr('y',
@@ -113,7 +170,7 @@ for (var j = 1; j <= m; j++) {
 	}
 
 	//draw those towers
-	towerArr[j - 1].draw(
+	towerArr[j - 1].drawTower(
 		svg, //svg argument
 		p.towerBuffer + (n * p.diskHeight) / 2, //attr x1
 		120, //attr x2
@@ -123,6 +180,10 @@ for (var j = 1; j <= m; j++) {
 	p.towerBuffer += 400
 }
 
+//set random colors for the disks
+d3.selectAll('.color').style('fill', function() {
+	return `hsl( ${Math.random() * 360}  ,100%,50%)`
+});
 
 //main event
 let game = new GameEngine()
@@ -130,57 +191,8 @@ let data = game.move(n, towerArr[0], towerArr[1], towerArr[2])
 console.log(`${game.count} moves`)
 console.log(data)
 
+// start animation
+game.animateDisk(n,p.diskHeight,p.animationDelay,p.animationDuration)
 
 
-//set random colors for the disks
-d3.selectAll('.color').style('fill', function() {
-	return `hsl( ${Math.random() * 360}  ,100%,50%)`
-});
 
-//get the distance between towers
-let get_distance = (dis1, dis2) => {
-	switch (true) {
-		case (dis1 === 'tower1' && dis2 === 'tower2') || (dis1 === 'tower2' && dis2 === 'tower3'):
-			return p.disNearestTower
-		case (dis1 === 'tower2' && dis2 === 'tower1') || (dis1 === 'tower3' && dis2 === 'tower2'):
-			return -p.disNearestTower
-		case (dis1 === 'tower1' && dis2 === 'tower3'):
-			return p.disFurthestTower
-		default:
-			return -p.disFurthestTower
-	}
-}
-
-//update the disks array in tower after being moved
-let update_disk = (disk, t1, t2) => {
-	t1.arrDisk.shift()
-	t2.arrDisk.unshift(disk)
-}
-
-//animate the disks
-for (var i = 0; i < data.length; i++) {
-	let x = get_distance(data[i].fromTower.name, data[i].toTower.name) // x = distence between the fromTower and the toTower
-	let begin_x = data[i].diskToPick.x_ //get the x_ of object disk 
-	let begin_y = data[i].diskToPick.y_ //get the y_ of object disk 
-	let countDisks = data[i].toTower.arrDisk.length // get the number of disks in the toTower 
-	let new_x = begin_x + x //set the new x_ (destination) for the disk
-	let new_y = n * p.diskHeight - (countDisks * p.diskHeight) - begin_y //set the new y_ for the disk
-	let height = -data[i].diskToPick.height //set the direction to move the disk,minus for up,plus for down
-
-	//update the current disks on each towers
-	update_disk(data[i].diskToPick, data[i].fromTower, data[i].toTower)
-
-	//the actual animation
-	d3.selectAll('.' + data[i].diskToPick.name)
-		.transition()
-		.delay(i * p.animationDelay)
-		.duration(p.animationDuration)
-		.attr('transform', 'translate(' + begin_x + ',' + height + ')')
-		.transition()
-		.attr('transform', 'translate(' + new_x + ',' + height + ')')
-		.transition()
-		.attr('transform', 'translate(' + new_x + ',' + new_y + ')')
-
-	//update the x ordinate of the disks after being moved
-	data[i].diskToPick.x_ += x
-}
